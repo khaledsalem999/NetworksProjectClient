4b12df5ac622b01b0b5e6d38761fe4d0cfa29134
khaledsalem999 <khaledsalem999@gmail.com>
2017-04-30 21:14:20 +0200

    Log off and shut down

--- FileSendClient/Form1.cs
+++ FileSendClient/Form1.cs
@@ -124,7 +124,7 @@ namespace FileSendClient
             Byte[] dataByte = new Byte[blockSize];
             lock (this)
             {
-                Stream fileStream = File.OpenWrite(@"C:\Users\user\networks2\" + fileName);
+                Stream fileStream = File.OpenWrite(@"C:\Users\Khaled\Desktop\Recievefiles" + fileName);
                 while (true)
                 {
                     thisRead = networkStream.Read(dataByte, 0, blockSize);
@@ -134,6 +134,28 @@ namespace FileSendClient
                 fileStream.Close();
             }
             return;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.clientSocket = new TcpClient("127.0.0.1", 8080);
+            String str = "SHUT_DOWN";
+            NetworkStream networkStream = clientSocket.GetStream();
+            this.sendString(networkStream, str);
+            byte[] bytesToRead = new byte[clientSocket.ReceiveBufferSize];
+            int bytesRead = networkStream.Read(bytesToRead, 0, clientSocket.ReceiveBufferSize);
+            string response = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.clientSocket = new TcpClient("127.0.0.1", 8080);
+            String str = "LOG_OFF";
+            NetworkStream networkStream = clientSocket.GetStream();
+            this.sendString(networkStream, str);
+            byte[] bytesToRead = new byte[clientSocket.ReceiveBufferSize];
+            int bytesRead = networkStream.Read(bytesToRead, 0, clientSocket.ReceiveBufferSize);
+            string response = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
         }
     }
 }
