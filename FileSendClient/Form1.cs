4691b3cf785a5d167d6512220c9a52c1d5df4467
Peldorri <passant.eldorry@gmail.com>
2017-05-01 17:03:34 +0200

    Delete Send Files

--- FileSendClient/Form1.cs
+++ FileSendClient/Form1.cs
@@ -22,7 +22,6 @@ namespace FileSendClient
         public TcpClient clientSocket;
         public TcpListener Listener;
         public NetworkStream networkStream;
-
         public Form1()
         {
             InitializeComponent();
@@ -38,24 +37,35 @@ namespace FileSendClient
         {
             try
             {
+                this.clientSocket = new TcpClient(textBox2.Text, 8080);
                 Stream fileStream = File.OpenRead(textBox1.Text);
                 // Alocate memory space for the file
-
-               // String cmd = "COMMAND_SEND:" + fileName;
-                byte[] fileBuffer = new byte[fileStream.Length];
-                fileStream.Read(fileBuffer, 0, (int)fileStream.Length);
-                // Open a TCP/IP Connection and send the data
+                String fileName = Path.GetFileName(textBox1.Text);
+                String cmd = "COMMAND_SEND:" + fileName;
                 NetworkStream networkStream = clientSocket.GetStream();
-                networkStream.Write(fileBuffer, 0, fileBuffer.GetLength(0));
+                this.sendString(networkStream, cmd);
                 byte[] bytesToRead = new byte[clientSocket.ReceiveBufferSize];
                 int bytesRead = networkStream.Read(bytesToRead, 0, clientSocket.ReceiveBufferSize);
                 string response = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+                if (response == "COMMAND_SEND_ACKNOWLEGED")
+                {
+                    // Open a TCP/IP Connection and send the data
+                    // NetworkStream networkStream = clientSocket.GetStream();
+                    byte[] fileBuffer = new byte[fileStream.Length];
+                    fileStream.Read(fileBuffer, 0, (int)fileStream.Length);
+                    networkStream.Write(fileBuffer, 0, fileBuffer.GetLength(0));
+                    networkStream.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Please Auth first");
+                }
             }
             catch (Exception f)
             {
                 MessageBox.Show("The target machine is not working");
             }
-            networkStream.Close();
+            
         }
         public void sendString(NetworkStream ns, string str)
         {
@@ -64,7 +74,7 @@ namespace FileSendClient
         }
         private void button3_Click(object sender, EventArgs e)
         {
-            this.clientSocket = new TcpClient("127.0.0.1", 8080);
+            this.clientSocket = new TcpClient(textBox2.Text, 8080);
             String str = "COMMAND_AUTH:"+textBox3.Text;
             NetworkStream networkStream = clientSocket.GetStream();
             this.sendString(networkStream, str);
@@ -77,7 +87,7 @@ namespace FileSendClient
         {
             try
             {
-                this.clientSocket = new TcpClient("127.0.0.1", 8080);
+                this.clientSocket = new TcpClient(textBox2.Text, 8080);
                 String str = "COMMAND_LIST";
                 NetworkStream networkStream = clientSocket.GetStream();  //betgeb el stream 3ashan a'dar ab3t ll server
                 this.sendString(networkStream, str);  //send el string 
@@ -87,6 +97,7 @@ namespace FileSendClient
                 if (!response.StartsWith("UNAUTH"))
                 {
                     string[] files = response.Split(',');
+                    fileList.Items.Clear();
                     foreach (var item in files)
                     {
                         fileList.Items.Add(item);
@@ -94,7 +105,7 @@ namespace FileSendClient
                 }
                 else
                 {
-                    MessageBox.Show("please authorize first");
+                    MessageBox.Show("Please authorize first");
                 }
             }
             catch (Exception f)
@@ -115,59 +126,121 @@ namespace FileSendClient
 
         private void fileList_SelectedIndexChanged(object sender, EventArgs e)
         {
-            this.clientSocket = new TcpClient("127.0.0.1", 8080);
-            String fileName = fileList.SelectedItem.ToString();
-            String cmd = "COMMAND_RECIEVE:"+ fileName;
-            NetworkStream networkStream = clientSocket.GetStream();  
-            this.sendString(networkStream, cmd);
-            int thisRead = 0;
-            int blockSize = 1024;
-            Byte[] dataByte = new Byte[blockSize];
-            lock (this)
+            try
             {
-                Stream fileStream = File.OpenWrite(@"C:\Users\Khaled\Desktop\Recievefiles\" + fileName);
-                while (true)
+                const string message =
+                                        "Which command you want to excute? Press (Yes to delete) (No to Recieve file) and (Cancel to abort) ";
+                const string caption = "Delete/Recieve File";
+                var result = MessageBox.Show(message, caption,
+                                             MessageBoxButtons.YesNoCancel,
+                                             MessageBoxIcon.Question);
+                String fileName = fileList.SelectedItem.ToString();
+
+                if (result == DialogResult.No)
                 {
-                    thisRead = networkStream.Read(dataByte, 0, blockSize);
-                    fileStream.Write(dataByte, 0, thisRead);
-                    if (thisRead == 0) break;
+                    this.clientSocket = new TcpClient(textBox2.Text, 8080);
+
+                    String cmd = "COMMAND_RECIEVE:" + fileName;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    this.sendString(networkStream, cmd);
+                    int thisRead = 0;
+                    int blockSize = 1024;
+                    Byte[] dataByte = new Byte[blockSize];
+                    lock (this)
+                    {
+                        Stream fileStream = File.OpenWrite(@"C:\Users\user\networks2\" + fileName);
+                        while (true)
+                        {
+                            thisRead = networkStream.Read(dataByte, 0, blockSize);
+                            fileStream.Write(dataByte, 0, thisRead);
+                            if (thisRead == 0) break;
+                        }
+                        fileStream.Close();
+                    }
                 }
-                fileStream.Close();
+                else if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        this.clientSocket = new TcpClient(textBox2.Text, 8080);
+                        String cmdD = "COMMAND_DELETE" + fileName;
+                        NetworkStream networkStream = clientSocket.GetStream();
+                        this.sendString(networkStream, cmdD);
+                        byte[] bytesToRead = new byte[clientSocket.ReceiveBufferSize];
+                        int bytesRead = networkStream.Read(bytesToRead, 0, clientSocket.ReceiveBufferSize);
+                        string response = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+                    }
+                    catch (Exception t)
+                    {
+                        MessageBox.Show("smth went wrong" + t);
+                    }
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception s)
+            {
+                MessageBox.Show("Target machine is not running");
             }
             return;
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            this.clientSocket = new TcpClient("127.0.0.1", 8080);
-            String str = "SHUT_DOWN";
-            NetworkStream networkStream = clientSocket.GetStream();
-            this.sendString(networkStream, str);
-            byte[] bytesToRead = new byte[clientSocket.ReceiveBufferSize];
-            int bytesRead = networkStream.Read(bytesToRead, 0, clientSocket.ReceiveBufferSize);
-            string response = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+            try
+            {
+                    this.clientSocket = new TcpClient(textBox2.Text, 8080);
+                    String str = "SHUT_DOWN";
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    this.sendString(networkStream, str);
+                    byte[] bytesToRead = new byte[clientSocket.ReceiveBufferSize];
+                    int bytesRead = networkStream.Read(bytesToRead, 0, clientSocket.ReceiveBufferSize);
+                    string response = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+               
+            }
+            catch(Exception a)
+            {
+                MessageBox.Show("Target machine is not running");
+            }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            this.clientSocket = new TcpClient("127.0.0.1", 8080);
-            String str = "LOG_OFF";
-            NetworkStream networkStream = clientSocket.GetStream();
-            this.sendString(networkStream, str);
-            byte[] bytesToRead = new byte[clientSocket.ReceiveBufferSize];
-            int bytesRead = networkStream.Read(bytesToRead, 0, clientSocket.ReceiveBufferSize);
-            string response = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+            try
+            {
+
+                this.clientSocket = new TcpClient(textBox2.Text, 8080);
+                String str = "LOG_OFF";
+                NetworkStream networkStream = clientSocket.GetStream();
+                this.sendString(networkStream, str);
+                byte[] bytesToRead = new byte[clientSocket.ReceiveBufferSize];
+                int bytesRead = networkStream.Read(bytesToRead, 0, clientSocket.ReceiveBufferSize);
+                string response = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+            }
+            catch(Exception w)
+            {
+                MessageBox.Show("Target machine is not running");
+            }
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            this.clientSocket = new TcpClient("127.0.0.1", 8080);
-            String str = "RESTART";
-            NetworkStream networkStream = clientSocket.GetStream();
-            this.sendString(networkStream, str);
-            byte[] bytesToRead = new byte[clientSocket.ReceiveBufferSize];
-            int bytesRead = networkStream.Read(bytesToRead, 0, clientSocket.ReceiveBufferSize);
-            string response = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+            try
+            {
+                this.clientSocket = new TcpClient(textBox2.Text, 8080);
+                String str = "RESTART";
+                NetworkStream networkStream = clientSocket.GetStream();
+                this.sendString(networkStream, str);
+                byte[] bytesToRead = new byte[clientSocket.ReceiveBufferSize];
+                int bytesRead = networkStream.Read(bytesToRead, 0, clientSocket.ReceiveBufferSize);
+                string response = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+            }
+            catch(Exception q)
+            {
+                MessageBox.Show("Target machine is not running");
+            }
         }
     }
 }
